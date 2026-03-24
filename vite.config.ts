67bfa6103bc51804608dbb32c6e10e4b435bbb08
Admin <211177151+kw6423@users.noreply.github.com>
2026-03-24 22:24:49 +0100

    Add vite port env

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,38 @@
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
-import { normalizePath } from "vite";
+import { normalizePath, loadEnv } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		tailwindcss(),
-		TanStackRouterVite(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: normalizePath(path.resolve("./src/assets/locales")),
-					dest: normalizePath(path.resolve("./dist")),
-				},
-			],
-		}),
-	],
-	server: {
-		host: true,
-		strictPort: true,
-	},
-	test: {
-		environment: "jsdom",
-		setupFiles: ["./vitest.setup.ts"],
-		css: true,
-	},
-});
+export default ({ mode }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [
+			react(),
+			tailwindcss(),
+			tanstackRouter(),
+			viteStaticCopy({
+				targets: [
+					{
+						src: normalizePath(path.resolve("./src/assets/locales")),
+						dest: normalizePath(path.resolve("./dist")),
+					},
+				],
+			}),
+		],
+		server: {
+			host: true,
+			port: Number(process.env.VITE_PORT),
+			strictPort: true,
+		},
+		test: {
+			environment: "jsdom",
+			setupFiles: ["./vitest.setup.ts"],
+			css: true,
+		},
+	});
+};
