096bd6bacac6afa112c7c44e89bb5c876bc5d452
Admin <211177151+kw6423@users.noreply.github.com>
2026-03-24 23:36:24 +0100

    Install commit organizer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -10,4 +10,23 @@ if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules 2>/dev/null)" ]; then
     pnpm install
 fi
 
+echo "Installing vscode codex commit organizer"
+
+RELEASE_URL="https://api.github.com/repos/kw6423/vscode-codex-commit-organizer-release/releases/latest"
+
+VSIX_URL="$(
+  curl -fsSL "$RELEASE_URL" |
+    grep browser_download_url |
+    grep '\.vsix' |
+    head -n1 |
+    cut -d '"' -f 4
+)"
+
+curl -fL "$VSIX_URL" -o /tmp/vscode-codex-commit-organizer.vsix
+
+# Do not name it code directly because it breaks some vsc extensions like Redis
+export resolved_code="$(ls ~/.vscode-server*/bin/*/bin/code-server* | head -n 1)"
+# Workaround as code is unavailable in postCreateCommand - https://github.com/microsoft/vscode-remote-release/issues/8535
+$resolved_code --install-extension /tmp/vscode-codex-commit-organizer.vsix --force
+
 echo "Devcontainer started"
