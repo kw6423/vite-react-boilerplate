301d2f295d75e7a668189407c22453abf23581ec
Admin <211177151+kw6423@users.noreply.github.com>
2026-03-24 22:55:18 +0100

    Auto pnpm install in devcontainer if no node modules

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules 2>/dev/null)" ]; then
+    echo "No node_modules folder detected, running pnpm install"
+    pnpm install
+fi
+
 echo "Devcontainer started"
